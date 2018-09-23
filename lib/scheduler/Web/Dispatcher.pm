9a86e09913c42423c429ee95e23d22095fed7713
liang_x <liang33x@gmail.com>
2018-09-23 17:04:56 +0900

    learn about amon

--- lib/scheduler/Web/Dispatcher.pm
+++ lib/scheduler/Web/Dispatcher.pm
@@ -3,27 +3,50 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
+use Time::Piece;
 
-any '/' => sub {
+get '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
+    my @schedules = $c->db->search('schedules', {}, { order_by => 'date DESC'});
+    return $c->render('index.tx', { schedules => \@schedules });
+};
+
+post '/post' => sub {
+    my ($c) = @_;
+    
+    my $title = $c->req->parameters->{title};
+    my $date = $c->req->parameters->{date};
+    
+    my $date_epoch = Time::Piece->strptime($date, '%Y/%m/%d')->epoch;
+    
+    $c->db->insert(schedules => {
+        title => $title,
+        date => $date_epoch,
     });
-};
-
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
+    
     return $c->redirect('/');
 };
 
-post '/account/logout' => sub {
+post '/schedules/:id/delete' => sub {
+    my ($c, $args) = @_;
+    my $id = $args->{id};
+
+    $c->db->delete('schedules' => { id => $id });
+    return $c->redirect('/');
+};
+
+get '/user' => sub {
     my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+    my $name = "JetStream SAM";
+    my $age = 21;
+    my $university = "Skipped University";
+    my $hobby = "Killing";
+    return $c->render('user.tx', {
+        name => $name,
+        age => $age,
+        university => $university,
+        hobby => $hobby,
+    });
 };
 
 1;
